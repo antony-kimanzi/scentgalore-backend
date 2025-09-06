25a1d2da943187691d6ee84c3a1357a97f11e94b
antony-kimanzi <antonykimanzi29@gmail.com>
2025-09-06 11:34:36 +0300

    added migrations in starting server

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
+import executeMigrations from "./migrations/runMigrations.js";
 
 dotenv.config();
 
-const app =  express();
+const app = express();
 
 // MIDDLEWARE //
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-const port = process.env.PORT || 3001;
+async function startServer() {
+  try {
+    if (process.env.NODE_ENV !== "production") executeMigrations();
 
-app.listen(port, () => console.log(`Server is running in port ${port}`))
+    const port = process.env.PORT || 3001;
+
+    app.listen(port, () => console.log(`Server is running in port ${port}`));
+    
+  } catch (error) {
+    console.log("failed to start server:", error);
+    process.exit(1);
+  }
+}
