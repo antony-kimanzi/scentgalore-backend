a78c3d827f25d673124e4271d2bd9348c041acea
antony-kimanzi <antonykimanzi29@gmail.com>
2025-09-11 13:18:44 +0300

    remove prisma instance import

--- utils/validation.js
+++ utils/validation.js
@@ -1,5 +1,3 @@
-import prisma from "../lib/prisma.js";
-
 export const sanitizeInput = (data) => {
   if (typeof data === "string") {
     return data.trim().replace(/[<>]/g, "");
@@ -17,7 +15,11 @@ export const sanitizeInput = (data) => {
   return data;
 };
 
-export const validateEmailUnique = async (email, excludeUserId = null) => {
+export const validateEmailUnique = async (
+  prisma,
+  email,
+  excludeUserId = null
+) => {
   const existingUser = await prisma.user.findUnique({ where: { email } });
 
   if (existingUser && existingUser[id] != excludeUserId) {
