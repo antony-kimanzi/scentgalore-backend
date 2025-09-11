f25f391d24ba77963c836a006d98d2a96a3d3d98
antony-kimanzi <antonykimanzi29@gmail.com>
2025-09-11 10:51:51 +0300

    remove validate request function

--- utils/validation.js
+++ utils/validation.js
@@ -19,33 +19,6 @@ export const sanitizeInput = (data) => {
   return data;
 };
 
-export const validateAndSanitize = (schema, data) => {
-  try {
-    const sanitizeData = sanitizeInput(data);
-    const validateData = schema.parse(sanitizeData);
-    return {
-      success: true,
-      data: validateData,
-      error: null,
-    };
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      const err = error.errors.map((err) => ({
-        fields: err.path.join("."),
-        message: err.message,
-      }));
-
-      return {
-        success: false,
-        data: null,
-        err,
-      };
-    }
-
-    throw error;
-  }
-};
-
 export const checkEmailUnique = async (email, excludeUserId = null) => {
   const existingUser = await prisma.user.findUnique({ where: { email } });
 
