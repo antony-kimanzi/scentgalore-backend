bbb34d36bd7722620e43614e613bfcd5ab668be5
antony-kimanzi <antonykimanzi29@gmail.com>
2025-09-20 17:51:09 +0300

    remove the id params schema

--- schema/userSchema.js
+++ schema/userSchema.js
@@ -49,11 +49,3 @@ export const loginSchema = z.object({
 });
 
 export const updateUserSchema = userSchema.partial();
-
-export const idParamSchema = z.object({
-  id: z
-    .string()
-    .regex(/^\d+$/, "ID must be a numeric value")
-    .transform((id) => parseInt(id, 10))
-    .refine((id) => id > 0, "ID must be a positive integer"),
-});
