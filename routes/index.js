d644abdb8c9cd5e30b06893924d9bfe15db3071d
antony-kimanzi <antonykimanzi29@gmail.com>
2025-09-22 17:07:21 +0300

    add main route for order

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import userRoutes from "./user.js";
 import authRoutes from "./auth.js";
 import productRoutes from "./product.js";
 import cartRoutes from "./cart.js";
+import orderRoutes from "./order.js";
 import { Router } from "express";
 
 const router = Router();
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/product", productRoutes);
 router.use("/cart", cartRoutes);
+router.use("/order", orderRoutes);
 
 export default router;
